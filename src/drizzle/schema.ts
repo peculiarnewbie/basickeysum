6f232e44474c40fbe535691107279a36225e2f0e
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-08-02 21:44:10 +0700

    proper addition and set

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,6 +1,9 @@
 import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
 
-export const keysums = sqliteTable("foo", {
+export const keysums = sqliteTable("keysums", {
 	key: text("key").notNull().primaryKey(),
 	sum: integer("sum").notNull().default(0),
 });
+
+export type SelectKeysums = typeof keysums.$inferSelect;
+export type InsertKeysums = typeof keysums.$inferInsert;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { Hono } from "hono";
 import { getDB } from "./drizzle/db";
 import { keysums } from "./drizzle/schema";
-import { AnyColumn, sql } from "drizzle-orm";
+import { AnyColumn, eq, inArray, sql } from "drizzle-orm";
 
 type Bindings = {
 	TURSO_DB_URL: string;
 	TURSO_DB_TOKEN: string;
-};
-
-const increment = (column: AnyColumn, value = 1) => {
-	return sql`${column} + ${value}`;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -26,22 +22,116 @@ app.get("/keysum", async (c) => {
 	return c.json(res);
 });
 
+app.get("/keysum/post", async (c) => {
+	return c.html(`
+		<form method="post" action="/keysum/post/form">
+			<input type="text" name="key" />
+			<input type="number" name="sum" />
+			<button type="submit">Submit</button>
+		</form>`);
+});
+
+app.post("/keysum/post/form", async (c) => {
+	const body = await c.req.parseBody();
+
+	console.log("on form post", body);
+	const { key, sum } = body;
+
+	const values = {};
+	//@ts-expect-error
+	values[key] = sum;
+
+	const url = new URL(c.req.url);
+	const postURL = url.origin + "/keysum/post";
+
+	const res = await fetch(postURL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(values),
+	});
+
+	if (res.status !== 200) {
+		return c.text("Error");
+	}
+
+	return c.json(res);
+});
+
+app.get("/keysum/:key", async (c) => {
+	const db = getDB(c.env.TURSO_DB_URL, c.env.TURSO_DB_TOKEN);
+
+	const res = await db
+		.select()
+		.from(keysums)
+		.where(eq(keysums.key, c.req.param("key")))
+		.all();
+
+	return c.json(res);
+});
+
 app.post("/keysum/post", async (c) => {
 	const body = await c.req.json();
+	console.log(body);
 	const pairs: [string, number][] = Object.entries(body);
-	console.log(pairs);
+	const keys = pairs.map(([key]) => key);
 
 	const db = getDB(c.env.TURSO_DB_URL, c.env.TURSO_DB_TOKEN);
 
-	const values = pairs.map(([key, sum]) => ({ key, sum }));
+	const currentValues = await db
+		.select()
+		.from(keysums)
+		.where(inArray(keysums.key, keys))
+		.all();
+
+	const values = pairs.map(([key, sum]) => {
+		const currentValue = currentValues.find(({ key: k }) => k === key)?.sum;
+		const value = currentValue ? currentValue + sum : sum;
+		return {
+			key: key,
+			sum: value,
+		};
+	});
 
 	const res = await db
 		.insert(keysums)
 		.values(values)
 		.onConflictDoUpdate({
 			target: keysums.key,
-			set: { sum: sql`${keysums.sum} + excluded.${keysums.sum}` },
-		});
+			set: {
+				sum: sql.raw(`excluded.sum`),
+			},
+		})
+		.returning();
+
+	console.log("res", res);
+
+	return c.json(res);
+});
+
+app.patch("keysum/post", async (c) => {
+	const body = await c.req.json();
+	console.log(body);
+	const pairs: [string, number][] = Object.entries(body);
+	const keys = pairs.map(([key]) => key);
+
+	const values = pairs.map(([key, sum]) => ({ key: key, sum: sum }));
+
+	const db = getDB(c.env.TURSO_DB_URL, c.env.TURSO_DB_TOKEN);
+
+	const res = await db
+		.insert(keysums)
+		.values(values)
+		.onConflictDoUpdate({
+			target: keysums.key,
+			set: {
+				sum: sql.raw(`excluded.sum`),
+			},
+		})
+		.returning();
+
+	return c.json(res);
 });
 
 export default app;
